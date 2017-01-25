65477164bb1887b2cc55146da5558ff8c49125a3
TheRicardo311 <r.vanoldenbarneveld@live.nl>
2017-01-25 12:06:42 +0100

    Class boat

---
+++ Ricardini/Boat.py
@@ -0,0 +1,53 @@
+class Boat:
+    def __init__(self,name,team,length,x,y):
+        self.name = name
+        self.team = team
+        self.length = length
+        self.hp = length
+        self.firepower = 1
+        self.offensive = True
+
+        #Range lengte defineren
+        if offensive == True:
+            #Horizontal + Vertical range!
+            range = length
+        else:
+            #Vertical only
+            range = length + 1
+
+
+
+        #Movement defineren
+        if offensive == True:
+            if length == 3:
+                self.movement = 3
+            if length == 4:
+                self.movement = 2
+            if length == 5:
+                self.movement = 1
+        else:
+            self.movement = 0
+
+    def PosSwitch(self, offensive):
+        if offensive == True:
+                offensive = False
+        else:
+                offensive = True
+
+    def RIPship:
+        if self.name = ... and hp = 0
+
+        elif self.name = ... and hp = 0
+
+        elif self.name = ... and hp = 0
+
+        elif self.name = ... and hp = 0
+
+        elif self.name = ... and hp = 0
+
+        elif self.name = ... and hp = 0
+
+        elif self.name = ... and hp = 0
+
+        elif self.name = ... and hp = 0
+
