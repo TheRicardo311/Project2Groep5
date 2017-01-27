5c1df690c765b55dc12e1f40b9e132a5c254f0fd
TheRicardo311 <r.vanoldenbarneveld@live.nl>
2017-01-27 10:18:13 +0100

    Cards and all that stuff

--- Ricardini/BoatUpdated.py
+++ Ricardini/BoatUpdated.py
@@ -60,110 +60,9 @@ class Boat:
 
 
 
-    class Cards
+class Cards
 
-        #OffensieveKaarten
-        def FMJ_Upgrade(self):
-            ship.firepower += 1
-            return ship.firepower
-        FMJ_Upgrade = FMJ_Upgrade()
 
-        def Rifling(self):
-            ship.range += 1
-            return ship.range
-        Rifling = Rifling()
-
-        def Advanced_Rifling(self):
-            ship.range += 1
-            return ship.range
-        Advanced_Rifling = Advanced_Rifling()
-
-        def Neval_Mine(self):
-
-            return
-        Neval_Mine = Neval_Mine()
-        def EMP_Upgrade(self):
-
-            return
-        EMP_Upgrade = EMP_Upgrade()
-
-        #DefensieveKaarten
-        def Reinforced_Hull(self):
-            ship.hp += 1
-            return ship.hp
-        Reinforced_Hull = Reinforced_Hull()
-
-        def Sonar(self):
-
-            return
-        Sonar = Sonar()
-
-        def Smokescreen(self):
-
-            return
-        Smokescreen = Smokescreen()
-
-        def Sabotage(self):
-            ship.hp -=1
-            return ship.hp()
-        Sabotage = Sabotage()
-
-        #HulpKaarten
-        def Backup(self):
-
-            return
-        Backup = Backup()
-
-        def Extra_Fuel(self):
-            ship.movement += 1
-            return ship.movement
-        Extra_Fuel = Extra_Fuel()
-
-        def Extra_Fuel2(self):
-            ship.movement += 2
-            return ship.movement
-        Extra_Fuel2 = Extra_Fuel2()
-
-        def Rally(self):
-            ship.movement += 1
-            return ship.movement
-        Rally = Rally()
-
-        def Adrenaline_Rush(self):
-
-            return
-        Adrenaline_Rush = Adrenaline_Rush()
-
-        #SpeciaalKaarten
-        def Repair(self):
-            ship.hp = ship.maxhp
-            return ship.hp
-        Repair = Repair()
-
-        def Flak_Armor(self):
-
-            return
-        Flak_Armor = Flak_Armor()
-
-        def Hack_Intel(self):
-
-            return
-        Hack_Intel = Hack_Intel()
-
-        def Far_Sight(self):
-            ship.range += 2
-            return ship.range
-        Far_Sight = Far_Sight()
-
-        def Aluminium_Hull(self):
-            ship.movement *= 2
-            return ship.movement
-        Aluminium_Hull = Aluminium_Hull()
-
-        def Jack_Sparrow(self):
-
-            return
-        Jack_Sparrow = Jack_Sparrow()
 
 
         def reset(self):
@@ -172,7 +71,146 @@ class Boat:
             self.range = self.maxrange
 
 
-taart = [1,2,3,4,5,6,7,8,9,10]
 
-a = randomint(taart)
-print(a)
+
+class Player:
+
+    def __init__(self, name, kleur):
+        self.name = name
+        self.score = 0
+        self.kleur = kleur
+
+        self.Furgo = Boat(2, 3, 2, 1)
+        self.Intensity = Boat(3, 2, 3, 1)
+        self.Silver = Boat(3, 2, 3, 1,)
+        self.Merapi = Boat(4, 1, 4, 1,)
+
+        self.Cards = []
+
+        # OffensieveKaarten
+        def FMJ_Upgrade(self):
+            ship.firepower += 1
+            return ship.firepower
+
+        self.FMJ_Upgrade = FMJ_Upgrade()
+
+        def Rifling(self):
+            ship.range += 1
+            return ship.range
+
+        self.Rifling = Rifling()
+
+        def Advanced_Rifling(self):
+            ship.range += 1
+            return ship.range
+
+        self.Advanced_Rifling = Advanced_Rifling()
+
+        def Neval_Mine(self):
+            return
+
+        self.Neval_Mine = Neval_Mine()
+
+        def EMP_Upgrade(self):
+            return
+
+        self.EMP_Upgrade = EMP_Upgrade()
+
+        # DefensieveKaarten
+        def Reinforced_Hull(self):
+            ship.hp += 1
+            return ship.hp
+
+        self.Reinforced_Hull = Reinforced_Hull()
+
+        def Sonar(self):
+            return
+
+        self.Sonar = Sonar()
+
+        def Smokescreen(self):
+            return
+
+        self.Smokescreen = Smokescreen()
+
+        def Sabotage(self):
+            ship.hp -= 1
+            return ship.hp()
+
+        self.Sabotage = Sabotage()
+
+        # HulpKaarten
+        def Backup(self):
+            return
+
+        self.Backup = Backup()
+
+        def Extra_Fuel(self):
+            ship.movement += 1
+            return ship.movement
+
+        self.Extra_Fuel = Extra_Fuel()
+
+        def Extra_Fuel2(self):
+            ship.movement += 2
+            return ship.movement
+
+        self.Extra_Fuel2 = Extra_Fuel2()
+
+        def Rally(self):
+            ship.movement += 1
+            return ship.movement
+
+        self.Rally = Rally()
+
+        def Adrenaline_Rush(self):
+            return
+
+        self.Adrenaline_Rush = Adrenaline_Rush()
+
+        # SpeciaalKaarten
+        def Repair(self):
+            ship.hp = ship.maxhp
+            return ship.hp
+
+        self.Repair = Repair()
+
+        def Flak_Armor(self):
+            return
+
+        self.Flak_Armor = Flak_Armor()
+
+        def Hack_Intel(self):
+            return
+
+        self.Hack_Intel = Hack_Intel()
+
+        def Far_Sight(self):
+            ship.range += 2
+            return ship.range
+
+        self.Far_Sight = Far_Sight()
+
+        def Aluminium_Hull(self):
+            ship.movement *= 2
+            return ship.movement
+
+        self.Aluminium_Hull = Aluminium_Hull()
+
+        def Jack_Sparrow(self):
+            return
+
+        self.Jack_Sparrow = Jack_Sparrow()
+
+    Card_Deck = [self.FMJ_Upgrade, self.FMJ_Upgrade, self.Rifling, self.Rifling, self.Advanced_Rifling, self.Advanced_Rifling, self.Neval_Mine, self.Neval_Mine, self.Neval_Mine, self.Neval_Mine, self.Neval_Mine, self.eval_Mine, self.EMP_Upgrade, self.EMP_Upgrade, self.EMP_Upgrade, self.EMP_Upgrade, self.Reinforced_Hull, self.Reinforced_Hull, self.Sonar, self.Sonar, self.Sonar, self.Sonar, self.Smokescreen, self.Smokescreen, self.Sabotage, self.Sabotage, self.Backup, self.Backup, self.Extra_Fuel, self.Extra_Fuel, self.Extra_Fuel, self.Extra_Fuel, self.Extra_Fuel, self.Extra_Fuel, self.Extra_Fuel2, self.Extra_Fuel2, self.Extra_Fuel2, self.Extra_Fuel2, self.Rally, self.Adrenaline_Rush, self.Adrenaline_Rush, self.Adrenaline_Rush, self.Adrenaline_Rush]
+    def Draw_Card(self, Card_Deck):
+        if len(Card_Deck) == 0:
+            Card_Deck = [self.FMJ_Upgrade, self.FMJ_Upgrade, self.Rifling, self.Rifling, self.Advanced_Rifling, self.Advanced_Rifling, self.Neval_Mine, self.Neval_Mine, self.Neval_Mine, self.Neval_Mine, self.Neval_Mine, self.eval_Mine, self.EMP_Upgrade, self.EMP_Upgrade, self.EMP_Upgrade, self.EMP_Upgrade, self.Reinforced_Hull, self.Reinforced_Hull, self.Sonar, self.Sonar, self.Sonar, self.Sonar, self.Smokescreen, self.Smokescreen, self.Sabotage, self.Sabotage, self.Backup, self.Backup, self.Extra_Fuel, self.Extra_Fuel, self.Extra_Fuel, self.Extra_Fuel, self.Extra_Fuel, self.Extra_Fuel, self.Extra_Fuel2, self.Extra_Fuel2, self.Extra_Fuel2, self.Extra_Fuel2, self.Rally, self.Adrenaline_Rush, self.Adrenaline_Rush, self.Adrenaline_Rush, self.Adrenaline_Rush]
+            Random_Card = random.choice(Card_Deck)
+            print(Random_Card)
+            Card_Deck.remove(Random_Card)
+
+            self.Cards.add(Random_Card)
+            print(self.Cards)
+            return Card_Deck
+    Card_Deck = Draw_Card(Card_Deck)
--- Ricardini/Draw_Card.py
+++ Ricardini/Draw_Card.py
@@ -1,14 +1,30 @@
 import random
 import pygame
 
-Card_Deck = [FMJ_Upgrade, FMJ_Upgrade, Rifling, Rifling, Advanced_Rifling, Advanced_Rifling, Neval_Mine, Neval_Mine, Neval_Mine, Neval_Mine, Neval_Mine, Neval_Mine, EMP_Upgrade, EMP_Upgrade, EMP_Upgrade, EMP_Upgrade, Reinforced_Hull, Reinforced_Hull, Sonar, Sonar, Sonar, Sonar, Smokescreen, Smokescreen, Sabotage, Sabotage, Backup, Backup, Extra_Fuel, Extra_Fuel, Extra_Fuel, Extra_Fuel, Extra_Fuel, Extra_Fuel, Extra_Fuel2, Extra_Fuel2, Extra_Fuel2, Extra_Fuel2, Rally, Adrenaline_Rush, Adrenaline_Rush, Adrenaline_Rush, Adrenaline_Rush]
+class Player:
 
-def Draw_Card(Card_Deck):
-    if len(Card_Deck) == 0:
-        Card_Deck = [FMJ_Upgrade, FMJ_Upgrade, Rifling, Rifling, Advanced_Rifling, Advanced_Rifling, Neval_Mine, Neval_Mine, Neval_Mine, Neval_Mine, Neval_Mine, Neval_Mine, EMP_Upgrade, EMP_Upgrade, EMP_Upgrade, EMP_Upgrade, Reinforced_Hull, Reinforced_Hull, Sonar, Sonar, Sonar, Sonar, Smokescreen, Smokescreen, Sabotage, Sabotage, Backup, Backup, Extra_Fuel, Extra_Fuel, Extra_Fuel, Extra_Fuel, Extra_Fuel, Extra_Fuel, Extra_Fuel2, Extra_Fuel2, Extra_Fuel2, Extra_Fuel2, Rally, Adrenaline_Rush, Adrenaline_Rush, Adrenaline_Rush, Adrenaline_Rush]
-    Random_Card = random.choice(Card_Deck)
-    print(Random_Card)
-    Card_Deck.remove(Random_Card)
-    return taart
+    def __init__(self, name, kleur):
+        self.name = name
+        self.score = 0
+        self.kleur = kleur
 
-Card_Deck = remove(Card_Deck)
+        self.Furgo = Boat(2, 3, 2, 1)
+        self.Intensity = Boat(3, 2, 3, 1)
+        self.silver = Boat(3, 2, 3, 1,)
+        self.Merapi = Boat(4, 1, 4, 1,)
+
+        self.Cards = []
+
+    Card_Deck = [FMJ_Upgrade, FMJ_Upgrade, Rifling, Rifling, Advanced_Rifling, Advanced_Rifling, Neval_Mine, Neval_Mine, Neval_Mine, Neval_Mine, Neval_Mine, Neval_Mine, EMP_Upgrade, EMP_Upgrade, EMP_Upgrade, EMP_Upgrade, Reinforced_Hull, Reinforced_Hull, Sonar, Sonar, Sonar, Sonar, Smokescreen, Smokescreen, Sabotage, Sabotage, Backup, Backup, Extra_Fuel, Extra_Fuel, Extra_Fuel, Extra_Fuel, Extra_Fuel, Extra_Fuel, Extra_Fuel2, Extra_Fuel2, Extra_Fuel2, Extra_Fuel2, Rally, Adrenaline_Rush, Adrenaline_Rush, Adrenaline_Rush, Adrenaline_Rush]
+
+    def Draw_Card(self, Card_Deck):
+        if len(Card_Deck) == 0:
+            Card_Deck = [FMJ_Upgrade, FMJ_Upgrade, Rifling, Rifling, Advanced_Rifling, Advanced_Rifling, Neval_Mine, Neval_Mine, Neval_Mine, Neval_Mine, Neval_Mine, Neval_Mine, EMP_Upgrade, EMP_Upgrade, EMP_Upgrade, EMP_Upgrade, Reinforced_Hull, Reinforced_Hull, Sonar, Sonar, Sonar, Sonar, Smokescreen, Smokescreen, Sabotage, Sabotage, Backup, Backup, Extra_Fuel, Extra_Fuel, Extra_Fuel, Extra_Fuel, Extra_Fuel, Extra_Fuel, Extra_Fuel2, Extra_Fuel2, Extra_Fuel2, Extra_Fuel2, Rally, Adrenaline_Rush, Adrenaline_Rush, Adrenaline_Rush, Adrenaline_Rush]
+            Random_Card = random.choice(Card_Deck)
+            print(Random_Card)
+            Card_Deck.remove(Random_Card)
+
+            self.Cards.add(Random_Card)
+            print(self.Cards)
+            return Card_Deck
+    Card_Deck = Draw_Card(Card_Deck)
