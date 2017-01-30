fd72fccfc4a17624d566aa0cc51ac1c5e5cacd65
TheRicardo311 <r.vanoldenbarneveld@live.nl>
2017-01-30 12:07:10 +0100

    Changes have been made boyos!

--- Ricardini/Draw_Card.py
+++ Ricardini/Draw_Card.py
@@ -20,11 +20,11 @@ class Player:
     def Draw_Card(self, Card_Deck):
         if len(Card_Deck) == 0:
             Card_Deck = [FMJ_Upgrade, FMJ_Upgrade, Rifling, Rifling, Advanced_Rifling, Advanced_Rifling, Neval_Mine, Neval_Mine, Neval_Mine, Neval_Mine, Neval_Mine, Neval_Mine, EMP_Upgrade, EMP_Upgrade, EMP_Upgrade, EMP_Upgrade, Reinforced_Hull, Reinforced_Hull, Sonar, Sonar, Sonar, Sonar, Smokescreen, Smokescreen, Sabotage, Sabotage, Backup, Backup, Extra_Fuel, Extra_Fuel, Extra_Fuel, Extra_Fuel, Extra_Fuel, Extra_Fuel, Extra_Fuel2, Extra_Fuel2, Extra_Fuel2, Extra_Fuel2, Rally, Adrenaline_Rush, Adrenaline_Rush, Adrenaline_Rush, Adrenaline_Rush]
-            Random_Card = random.choice(Card_Deck)
-            print(Random_Card)
-            Card_Deck.remove(Random_Card)
+        Random_Card = random.choice(Card_Deck)
+        print(Random_Card)
+        Card_Deck.remove(Random_Card)
 
-            self.Cards.add(Random_Card)
-            print(self.Cards)
-            return Card_Deck
+        self.Cards.add(Random_Card)
+        print(self.Cards)
+        return Card_Deck
     Card_Deck = Draw_Card(Card_Deck)
