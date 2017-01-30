6c0123be13ded1c314782369ea31b779785cdcf0
TheRicardo311 <r.vanoldenbarneveld@live.nl>
2017-01-30 12:06:49 +0100

    The sound of music

---
+++ Ricardini/Sounds.py
@@ -0,0 +1,7 @@
+import pygame
+
+pygame.init()
+pygame.mixer.init()
+pygame.mixer.music.load("C:/Users/Ricardo/Documents/Github/Project2Groep5/Ricardini/Sounds/Shot.ogg")
+pygame.mixer.music.play()
+
