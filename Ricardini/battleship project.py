22b82f5415116360b4c876941ba8defbd90bbee3
TheRicardo311 <r.vanoldenbarneveld@live.nl>
2017-01-30 15:34:17 +0100

    kleine fix

--- Ricardini/battleship project.py
+++ Ricardini/battleship project.py
@@ -266,22 +266,7 @@ class Menu:
             self.draw_termination()
 
     def BgMusic(self):
-        bgMusic = ['C:/Users/Ricardo/Documents/GitHub/Project2Groep5/Ricardini/8-bitMusic/Castlevania2-BloodyTears.ogg',
-                   'C:/Users/Ricardo/Documents/GitHub/Project2Groep5/Ricardini/8-bitMusic/Castlevania-SymphonyoftheNight-Dracula.ogg',
-                   'C:/Users/Ricardo/Documents/GitHub/Project2Groep5/Ricardini/8-bitMusic/FinalFantasyII-NFC--DeadMusic.ogg',
-                   'C:/Users/Ricardo/Documents/GitHub/Project2Groep5/Ricardini/8-bitMusic/FinalFantasyII-NFC--PandemoniumCastle.ogg',
-                   'C:/Users/Ricardo/Documents/GitHub/Project2Groep5/Ricardini/8-bitMusic/LeagueOfLegendsMusic-BitRush.ogg',
-                   'C:/Users/Ricardo/Documents/GitHub/Project2Groep5/Ricardini/8-bitMusic/LegendOfZeldaIntro.ogg',
-                   'C:/Users/Ricardo/Documents/GitHub/Project2Groep5/Ricardini/8-bitMusic/LegendOfZeldaTheme.ogg',
-                   'C:/Users/Ricardo/Documents/GitHub/Project2Groep5/Ricardini/8-bitMusic/MarioTheme.ogg',
-                   'C:/Users/Ricardo/Documents/GitHub/Project2Groep5/Ricardini/8-bitMusic/PokemonTheme.ogg',
-                   'C:/Users/Ricardo/Documents/GitHub/Project2Groep5/Ricardini/8-bitMusic/SkyrimTheme.ogg',
-                   'C:/Users/Ricardo/Documents/GitHub/Project2Groep5/Ricardini/8-bitMusic/Undertale-Megalovania.ogg',
-                   'C:/Users/Ricardo/Documents/GitHub/Project2Groep5/Ricardini/8-bitMusic/Undertale-SpiderDanceExtended.ogg',
-                   'C:/Users/Ricardo/Documents/GitHub/Project2Groep5/Ricardini/8-bitMusic/Zelda2-TheAdventureOfLink-Palace.ogg',
-                   'C:/Users/Ricardo/Documents/GitHub/Project2Groep5/Ricardini/8-bitMusic/bgmusic1.ogg',
-                   'C:/Users/Ricardo/Documents/GitHub/Project2Groep5/Ricardini/8-bitMusic/bgmusic2.ogg',
-                   'C:/Users/Ricardo/Documents/GitHub/Project2Groep5/Ricardini/8-bitMusic/bgmusic3.ogg']
+        bgMusic = ['C:/Users/Ricardo/Documents/GitHub/Project2Groep5/Ricardini/8-bitMusic/Castlevania2-BloodyTears.ogg', 'C:/Users/Ricardo/Documents/GitHub/Project2Groep5/Ricardini/8-bitMusic/Castlevania-SymphonyoftheNight-Dracula.ogg', 'C:/Users/Ricardo/Documents/GitHub/Project2Groep5/Ricardini/8-bitMusic/FinalFantasyII-NFC--DeadMusic.ogg', 'C:/Users/Ricardo/Documents/GitHub/Project2Groep5/Ricardini/8-bitMusic/FinalFantasyII-NFC--PandemoniumCastle.ogg', 'C:/Users/Ricardo/Documents/GitHub/Project2Groep5/Ricardini/8-bitMusic/LeagueOfLegendsMusic-BitRush.ogg', 'C:/Users/Ricardo/Documents/GitHub/Project2Groep5/Ricardini/8-bitMusic/LegendOfZeldaIntro.ogg', 'C:/Users/Ricardo/Documents/GitHub/Project2Groep5/Ricardini/8-bitMusic/LegendOfZeldaTheme.ogg', 'C:/Users/Ricardo/Documents/GitHub/Project2Groep5/Ricardini/8-bitMusic/MarioTheme.ogg', 'C:/Users/Ricardo/Documents/GitHub/Project2Groep5/Ricardini/8-bitMusic/PokemonTheme.ogg', 'C:/Users/Ricardo/Documents/GitHub/Project2Groep5/Ricardini/8-bitMusic/SkyrimTheme.ogg', 'C:/Users/Ricardo/Documents/GitHub/Project2Groep5/Ricardini/8-bitMusic/Undertale-Megalovania.ogg', 'C:/Users/Ricardo/Documents/GitHub/Project2Groep5/Ricardini/8-bitMusic/Undertale-SpiderDanceExtended.ogg', 'C:/Users/Ricardo/Documents/GitHub/Project2Groep5/Ricardini/8-bitMusic/Zelda2-TheAdventureOfLink-Palace.ogg', 'C:/Users/Ricardo/Documents/GitHub/Project2Groep5/Ricardini/8-bitMusic/bgmusic1.ogg', 'C:/Users/Ricardo/Documents/GitHub/Project2Groep5/Ricardini/8-bitMusic/bgmusic2.ogg', 'C:/Users/Ricardo/Documents/GitHub/Project2Groep5/Ricardini/8-bitMusic/bgmusic3.ogg']
 
         Random_bgMusic = random.choice(bgMusic)
         pygame.mixer.init()
