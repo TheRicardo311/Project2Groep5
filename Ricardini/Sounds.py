dcf6d10ca6d3d51ad8c0131a7be3536d487d1e26
TheRicardo311 <r.vanoldenbarneveld@live.nl>
2017-01-31 10:40:22 +0100

    Code for the sounds

--- Ricardini/Sounds.py
+++ Ricardini/Sounds.py
@@ -2,6 +2,25 @@ import pygame
 
 pygame.init()
 pygame.mixer.init()
-pygame.mixer.music.load("C:/Users/Ricardo/Documents/Github/Project2Groep5/Ricardini/Sounds/Shot.ogg")
+pygame.mixer.music.load('Movement.ogg')
 pygame.mixer.music.play()
 
+pygame.init()
+pygame.mixer.init()
+pygame.mixer.music.load('RIPShip.ogg')
+pygame.mixer.music.play()
+
+pygame.init()
+pygame.mixer.init()
+pygame.mixer.music.load('Shot.ogg')
+pygame.mixer.music.play()
+
+pygame.init()
+pygame.mixer.init()
+pygame.mixer.music.load('ShotHit.ogg')
+pygame.mixer.music.play()
+
+pygame.init()
+pygame.mixer.init()
+pygame.mixer.music.load('SwitchPos.ogg')
+pygame.mixer.music.play()
